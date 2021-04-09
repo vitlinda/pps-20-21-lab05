6f16c23575b75c2a5935a59e0428b3f69f379eeb
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-04-09 13:18:28 +0200

    test reduce

--- src/u05lab/code/Ex1-Lists.scala
+++ src/u05lab/code/Ex1-Lists.scala
@@ -21,26 +21,26 @@ sealed trait List[A] {
 
   def toSeq: Seq[A]
 
-  def foldLeft[B](acc: B)(f: (B,A)=>B): B
+  def foldLeft[B](acc: B)(f: (B, A) => B): B
 
-  def foldRight[B](acc: B)(f: (A,B)=>B): B
+  def foldRight[B](acc: B)(f: (A, B) => B): B
 
   def flatMap[B](f: A => List[B]): List[B]
 
   def reverse(): List[A]
 
-  def zipRight: List[(A,Int)]
+  def zipRight: List[(A, Int)]
 
-  def partition(pred: A => Boolean): (List[A],List[A])
+  def partition(pred: A => Boolean): (List[A], List[A])
 
-  def span(pred: A => Boolean): (List[A],List[A])
+  def span(pred: A => Boolean): (List[A], List[A])
 
-  def reduce(op: (A,A)=>A): A
+  def reduce(op: (A, A) => A): A
 
   def takeRight(n: Int): List[A]
 
   // right-associative construction: 10 :: 20 :: 30 :: Nil()
-  def ::(head: A): List[A] = Cons(head,this)
+  def ::(head: A): List[A] = Cons(head, this)
 }
 
 // defining concrete implementations based on the same template
@@ -54,8 +54,8 @@ case class Nil[A]()
 // enabling pattern matching on ::
 
 object :: {
-  def unapply[A](l: List[A]): Option[(A,List[A])] = l match {
-    case Cons(h,t) => Some((h,t))
+  def unapply[A](l: List[A]): Option[(A, List[A])] = l match {
+    case Cons(h, t) => Some((h, t))
     case _ => None
   }
 }
@@ -67,28 +67,37 @@ trait ListImplementation[A] extends List[A] {
     case h :: t => Some(h)
     case _ => None
   }
+
   override def tail: Option[List[A]] = this match {
     case h :: t => Some(t)
     case _ => None
   }
+
   override def append(list: List[A]): List[A] = this match {
     case h :: t => h :: (t append list)
     case _ => list
   }
-  override def foreach(consumer: (A)=>Unit): Unit = this match {
-    case h :: t => {consumer(h); t foreach consumer}
+
+  override def foreach(consumer: (A) => Unit): Unit = this match {
+    case h :: t => {
+      consumer(h);
+      t foreach consumer
+    }
     case _ => None
   }
+
   override def get(pos: Int): Option[A] = this match {
     case h :: t if pos == 0 => Some(h)
-    case h :: t if pos > 0 => t get (pos-1)
+    case h :: t if pos > 0 => t get (pos - 1)
     case _ => None
   }
+
   override def filter(predicate: (A) => Boolean): List[A] = this match {
     case h :: t if (predicate(h)) => h :: (t filter predicate)
     case _ :: t => (t filter predicate)
     case _ => Nil()
   }
+
   override def map[B](fun: (A) => B): List[B] = this match {
     case h :: t => fun(h) :: (t map fun)
     case _ => Nil()
@@ -99,24 +108,24 @@ trait ListImplementation[A] extends List[A] {
     case _ => Seq()
   }
 
-  override def foldLeft[B](acc: B)(f: (B,A)=>B): B = this match {
-    case Cons(h,t) => t.foldLeft(f(acc,h))(f)
+  override def foldLeft[B](acc: B)(f: (B, A) => B): B = this match {
+    case Cons(h, t) => t.foldLeft(f(acc, h))(f)
     case Nil() => acc
   }
 
   override def foldRight[B](acc: B)(f: (A, B) => B): B =
-    this.reverse().foldLeft(acc)((acc,elem) => f(elem,acc))
+    this.reverse().foldLeft(acc)((acc, elem) => f(elem, acc))
 
   override def reverse(): List[A] =
-    this.foldLeft(Nil[A].asInstanceOf[List[A]])((acc,elem) => Cons(elem,acc))
+    this.foldLeft(Nil[A].asInstanceOf[List[A]])((acc, elem) => Cons(elem, acc))
 
   override def flatMap[B](f: A => List[B]): List[B] = this match {
-    case Cons(h,t) => f(h).append(t.flatMap(f))
+    case Cons(h, t) => f(h).append(t.flatMap(f))
     case Nil() => Nil()
   }
 
   override def zipRight: List[(A, Int)] = {
-   /* @tailrec
+    /* @tailrec
     def _zipRight(l: List[A], k: Int = 0, res: List[(A, Int)] = List.nil): List[(A, Int)] = l match{
       case h :: t => _zipRight(t, k + 1, (h, k) :: res)
       case _ => res
@@ -124,28 +133,32 @@ trait ListImplementation[A] extends List[A] {
     _zipRight(this).reverse()
     */
     var k = 0
-    this.map(e => { val mapped = (e, k); k+=1; mapped })
+    this.map(e => {
+      val mapped = (e, k);
+      k += 1;
+      mapped
+    })
 
     /*
     * var k = -1
     * this.map(e => {k+=1;(e, k) })
     * */
     //provare soluzioni alternative
-   // oppure usare stream, con una map che itera contemporaneamente su due liste
+    // oppure usare stream, con una map che itera contemporaneamente su due liste
   }
 
-  override def partition(pred: A => Boolean): (List[A],List[A]) = {
+  override def partition(pred: A => Boolean): (List[A], List[A]) = {
     var l1: List[A] = Nil()
     var l2: List[A] = Nil()
-    foreach( h => if (pred(h)) l1 = h :: l1 else l2 = h :: l2)
+    foreach(h => if (pred(h)) l1 = h :: l1 else l2 = h :: l2)
     (l1.reverse(), l2.reverse())
   }
 
-  override def span(pred: A => Boolean): (List[A],List[A]) = {
+  override def span(pred: A => Boolean): (List[A], List[A]) = {
     var l1: List[A] = Nil()
     var list: List[A] = this
 
-    while(pred(list.head.get)){
+    while (list.head.isDefined && pred(list.head.get)) {
       l1 = list.head.get :: l1
       list = list.tail.get
     }
@@ -156,7 +169,11 @@ trait ListImplementation[A] extends List[A] {
     *
     * @throws UnsupportedOperationException if the list is empty
     */
-  override def reduce(op: (A,A)=>A): A = ???
+  override def reduce(op: (A, A) => A): A = this match {
+    case Cons(h, t) => op(h, t.reduce(op))
+    case Cons(h, Nil()) => h
+    case Nil() => throw new UnsupportedOperationException
+  }
 
   override def takeRight(n: Int): List[A] = ???
 }
@@ -166,51 +183,57 @@ object List {
 
   // Smart constructors
   def nil[A]: List[A] = Nil()
-  def cons[A](h: A, t: List[A]): List[A] = Cons(h,t)
+
+  def cons[A](h: A, t: List[A]): List[A] = Cons(h, t)
 
   def apply[A](elems: A*): List[A] = {
     var list: List[A] = Nil()
-    for (i <- elems.length-1 to 0 by -1) list = elems(i) :: list
+    for (i <- elems.length - 1 to 0 by -1) list = elems(i) :: list
     list
   }
 
   def of[A](elem: A, n: Int): List[A] =
-    if (n==0) Nil() else elem :: of(elem,n-1)
+    if (n == 0) Nil() else elem :: of(elem, n - 1)
 }
 
 object ListsTest extends App {
 
-  import List._  // Working with the above lists
-  println(List(10,20,30,40))
+  import List._ // Working with the above lists
+  println(List(10, 20, 30, 40))
   val l = 10 :: 20 :: 30 :: 40 :: Nil() // same as above
   println(l.head) // 10
   println(l.tail) // 20,30,40
   println(l append l) // 10,20,30,40,10,20,30,40
   println(l append l toSeq) // as a list: 10,20,30,40,10,20,30,40
   println(l get 2) // 30
-  println(of("a",10)) // a,a,a,..,a
-  println(l filter (_<=20) map ("a"+_) ) // a10, a20
+  println(of("a", 10)) // a,a,a,..,a
+  println(l filter (_ <= 20) map ("a" + _)) // a10, a20
 
-  assert(List(1,2,3) == List(1,2,3))
+  assert(List(1, 2, 3) == List(1, 2, 3))
 
   println("Scala partition")
-  println(scala.collection.immutable.List(10,20,30,40).partition(_>15))
+  println(scala.collection.immutable.List(10, 20, 30, 40).partition(_ > 15))
   println("Scala span")
-  println(scala.collection.immutable.List(10,20,30,40).span(_>15))
+  println(scala.collection.immutable.List(10, 20, 30, 40).span(_ > 15))
 
   println("Ex. 1: zipRight")
   println(l.zipRight.toSeq) // List((10,0), (20,1), (30,2), (40,3))
 
   println("Ex. 2: partition")
-  println(l.partition(_>15)) // ( Cons(20,Cons(30,Cons(40,Nil()))), Cons(10,Nil()) )
+  println(l.partition(_ > 15)) // ( Cons(20,Cons(30,Cons(40,Nil()))), Cons(10,Nil()) )
 
   println("Ex. 3: span")
-  println(l.span(_>15)) // ( Nil(), Cons(10,Cons(20,Cons(30,Cons(40,Nil())))) )
-  println(l.span(_<15)) // ( Cons(10,Nil()), Cons(20,Cons(30,Cons(40,Nil()))) )
+  println(l.span(_ > 15)) // ( Nil(), Cons(10,Cons(20,Cons(30,Cons(40,Nil())))) )
+  println(l.span(_ < 15)) // ( Cons(10,Nil()), Cons(20,Cons(30,Cons(40,Nil()))) )
 
   println("Ex. 4: reduce")
-  println(l.reduce(_+_)) // 100
-  try { List[Int]().reduce(_+_); assert(false) } catch { case _:UnsupportedOperationException => }
+  println(l.reduce(_ + _)) // 100
+  try {
+    List[Int]().reduce(_ + _);
+    assert(false)
+  } catch {
+    case _: UnsupportedOperationException =>
+  }
 
   println("Ex. 5: takeRight")
   println(l.takeRight(2)) // Cons(30,Cons(40,Nil()))
--- test/u05lab/ListsMyTest.scala
+++ test/u05lab/ListsMyTest.scala
@@ -16,15 +16,27 @@ class ListsMyTest {
   @Test
   def testPartition() ={
     import List._
-    val l = 10 :: 20 :: 30 :: 40 :: nil // same as above
+    val l = 10 :: 20 :: 30 :: 40 :: nil
     assertEquals((cons(20, cons(30, cons(40, nil))), cons(10, nil)), l.partition(_>15))
   }
 
   @Test
-  def testSpan() ={
+  def testSpan() = {
     import List._
-    val l = 10 :: 20 :: 30 :: 40 :: nil // same as above
+    val l = 10 :: 20 :: 30 :: 40 :: nil
     assertEquals((nil, cons(10, cons(20, cons(30, cons(40, nil))))), l.span(_>15))
     assertEquals((cons(10, nil), cons(20, cons(30, cons(40, nil)))), l.span(_<15))
+  }
+
+  @Test
+  def testReduce(): Unit = {
+    import List._
+    val l = 10 :: 20 :: 30 :: 40 :: nil
+    assertThrows(classOf[UnsupportedOperationException], () => List.nil[Int].reduce(_+_))
+//    assertEquals(100, l.reduce(_+_))
+//
+//    val l1 = 10 :: nil
+//    assertEquals(10, l.reduce(_+_))
+
   }
 }
