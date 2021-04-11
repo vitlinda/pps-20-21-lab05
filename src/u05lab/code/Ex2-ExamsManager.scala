47741cdb24ef3c0e23bbddf95eccfb4e876240b2
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-04-11 16:49:32 +0200

    exams manager tesst

--- src/u05lab/code/Ex2-ExamsManager.scala
+++ src/u05lab/code/Ex2-ExamsManager.scala
@@ -1,6 +1,59 @@
 package u05lab.code
 
+import scala.collection.immutable.HashMap
+
+
+trait ExamsManager {
+  def createNewCall(call: String): Unit
+
+  def addStudentResult(call: String, student: String, result: ExamResult): Unit
+
+  def getAllStudentsFromCall(call: String): Set[String]
+
+  def getEvaluationsMapFromCall(call: String): Map[String, Int]
+
+  def getResultsMapFromStudent(student: String): Map[String, String]
+
+  def getBestResultFromStudent(student: String): Option[Int]
+}
+
 object ExamsManager {
+  def apply(): ExamsManager = ExamsManagerImpl()
+
+  private case class ExamsManagerImpl() extends ExamsManager {
+    private var map: Map[String, Map[String, ExamResult]] = HashMap()
+    private def checkArgument(cond: Boolean): Unit = if(!cond) throw new IllegalArgumentException
+
+    override def createNewCall(call: String): Unit = {
+      checkArgument(!map.contains(call))
+      map = map + (call -> Map())
+    }
+
+    override def addStudentResult(call: String, student: String, result: ExamResult): Unit = {
+      checkArgument(map.contains(call))
+      checkArgument(!map(call).contains(student))
+      map = map + (call -> (map(call) + (student -> result)))
+    }
+
+    override def getAllStudentsFromCall(call: String): Set[String] = map(call).keySet
+
+    override def getEvaluationsMapFromCall(call: String): Map[String, Int] = {
+      map(call).filter(_._2.evaluation.isDefined).map(v => (v._1 -> v._2.evaluation.get))
+    }
+
+    override def getResultsMapFromStudent(student: String): Map[String, String] = {
+      map.filter(p => p._2.contains(student)).map(e => (e._1 -> e._2(student).toString))
+    }
+
+    override def getBestResultFromStudent(student: String): Option[Int] = {
+      val results = map.values.flatten.filter(_._1 == student).filter(_._2.evaluation.isDefined).map(_._2.evaluation).toList
+      results match {
+        case scala.Nil => None
+        case _ => results.max
+      }
+    }
+
+  }
 
 }
 
@@ -25,24 +78,24 @@ trait ExamResult {
 }
 
 object ExamResult {
-  def apply(kind: Kind, evaluation: Option[Int], cumLaude: Boolean): ExamResult = ExamResultImpl(kind: Kind, evaluation, cumLaude)
+  def apply(kind: Kind, evaluation: Option[Int], cumLaude: Boolean): ExamResult = new ExamResultImpl(kind, evaluation, cumLaude)
 
-  case class ExamResultImpl(override val kind: Kind,
-                            override  val evaluation: Option[Int],
-                            override val cumLaude: Boolean) extends ExamResult {
+  class ExamResultImpl(override val kind: Kind,
+                       override val evaluation: Option[Int],
+                       override val cumLaude: Boolean) extends ExamResult {
     override def toString: String = kind.toString
   }
 
   case class ExamResultSucceeded(override val kind: Kind,
-                                 override  val evaluation: Option[Int],
-                                 override val cumLaude: Boolean) extends ExamResult {
-    override def toString: String = kind.toString + "(" + evaluation.get + ")"
+                                 override val evaluation: Option[Int],
+                                 override val cumLaude: Boolean) extends ExamResultImpl(kind, evaluation, cumLaude) {
+    override def toString: String = super.toString + "(" + evaluation.get + ")"
   }
 
   case class ExamResultSucceededCumLaude(override val kind: Kind,
-                                 override  val evaluation: Option[Int],
-                                 override val cumLaude: Boolean) extends ExamResult {
-    override def toString: String = kind.toString + "(" + evaluation.get + "L)"
+                                         override val evaluation: Option[Int],
+                                         override val cumLaude: Boolean) extends ExamResultImpl(kind, evaluation, cumLaude) {
+    override def toString: String = super.toString + "(" + evaluation.get + "L)"
   }
 
 }
@@ -63,9 +116,11 @@ class ExamResultFactoryImpl() extends ExamResultFactory {
   override def retired(): ExamResult = ExamResult(Kind.RETIRED, None, false)
 
   import u05lab.code.ExamResult.ExamResultSucceededCumLaude
+
   override def succeededCumLaude(): ExamResult = ExamResultSucceededCumLaude(Kind.SUCCEEDED, Some(30), true)
 
   import u05lab.code.ExamResult.ExamResultSucceeded
+
   override def succeeded(evaluation: Int): ExamResult = {
     if (evaluation < 18 || evaluation > 30) throw new IllegalArgumentException
     ExamResultSucceeded(Kind.SUCCEEDED, Some(evaluation), false)
--- test/u05lab/ExamsManagerTest.scala
+++ test/u05lab/ExamsManagerTest.scala
@@ -1,12 +1,14 @@
 package u05lab
 import org.junit.Assert._
 import org.junit.Test
-import u05lab.code.{ExamResultFactoryImpl, Kind}
+import u05lab.code.{ExamResultFactoryImpl, ExamsManager, Kind}
+
+import scala.collection.immutable.HashSet
 
 
 class ExamsManagerTest {
   private val erf = new ExamResultFactoryImpl()
-//  private val em = new ExamsManagerImpl()
+  private val em: ExamsManager = ExamsManager()
 
   // verifica base di ExamResultFactory
   @Test
@@ -48,80 +50,82 @@ class ExamsManagerTest {
     erf.succeeded(17)
   }
 
-//  // metodo di creazione di una situazione di risultati in 3 appelli
-//  def prepareExams() {
-//    em.createNewCall("gennaio")
-//    em.createNewCall("febbraio")
-//    em.createNewCall("marzo")
-//
-//    em.addStudentResult("gennaio", "rossi", erf.failed()) // rossi -> fallito
-//    em.addStudentResult("gennaio", "bianchi", erf.retired()) // bianchi -> ritirato
-//    em.addStudentResult("gennaio", "verdi", erf.succeeded(28)) // verdi -> 28
-//    em.addStudentResult("gennaio", "neri", erf.succeededCumLaude()) // neri -> 30L
-//
-//    em.addStudentResult("febbraio", "rossi", erf.failed()) // etc..
-//    em.addStudentResult("febbraio", "bianchi", erf.succeeded(20))
-//    em.addStudentResult("febbraio", "verdi", erf.succeeded(30))
-//
-//    em.addStudentResult("marzo", "rossi", erf.succeeded(25))
-//    em.addStudentResult("marzo", "bianchi", erf.succeeded(25))
-//    em.addStudentResult("marzo", "viola", erf.failed())
-//  }
-//
-//  // verifica base della parte obbligatoria di ExamManager
-//  @Test
-//  def testExamsManagement() {
-//    this.prepareExams()
-//    // partecipanti agli appelli di gennaio e marzo
-//    assertEquals(em.getAllStudentsFromCall("gennaio"),new HashSet<>(List("rossi","bianchi","verdi","neri")))
-//    assertEquals(em.getAllStudentsFromCall("marzo"),new HashSet<>(List("rossi","bianchi","viola")))
-//
-//    // promossi di gennaio con voto
-//    assertEquals(em.getEvaluationsMapFromCall("gennaio").size(),2)
-//    assertEquals(em.getEvaluationsMapFromCall("gennaio").get("verdi").intValue(),28)
-//    assertEquals(em.getEvaluationsMapFromCall("gennaio").get("neri").intValue(),30)
-//    // promossi di febbraio con voto
-//    assertEquals(em.getEvaluationsMapFromCall("febbraio").size(),2);
-//    assertEquals(em.getEvaluationsMapFromCall("febbraio").get("bianchi").intValue(),20)
-//    assertEquals(em.getEvaluationsMapFromCall("febbraio").get("verdi").intValue(),30)
-//
-//    // tutti i risultati di rossi (attenzione ai toString!!)
-//    assertEquals(em.getResultsMapFromStudent("rossi").size(),3)
-//    assertEquals(em.getResultsMapFromStudent("rossi").get("gennaio"),"FAILED")
-//    assertEquals(em.getResultsMapFromStudent("rossi").get("febbraio"),"FAILED")
-//    assertEquals(em.getResultsMapFromStudent("rossi").get("marzo"),"SUCCEEDED(25)")
-//    // tutti i risultati di bianchi
-//    assertEquals(em.getResultsMapFromStudent("bianchi").size(),3)
-//    assertEquals(em.getResultsMapFromStudent("bianchi").get("gennaio"),"RETIRED")
-//    assertEquals(em.getResultsMapFromStudent("bianchi").get("febbraio"),"SUCCEEDED(20)")
-//    assertEquals(em.getResultsMapFromStudent("bianchi").get("marzo"),"SUCCEEDED(25)")
-//    // tutti i risultati di neri
-//    assertEquals(em.getResultsMapFromStudent("neri").size(),1)
-//    assertEquals(em.getResultsMapFromStudent("neri").get("gennaio"),"SUCCEEDED(30L)")
-//
-//  }
-//
-//  // verifica del metodo ExamManager.getBestResultFromStudent
-//  @Test
-//  def optionalTestExamsManagement() {
-//    this.prepareExams();
-//    // miglior voto acquisito da ogni studente, o vuoto..
-//    assertEquals(em.getBestResultFromStudent("rossi"), Some(25))
-//    assertEquals(em.getBestResultFromStudent("bianchi"), Some(25))
-//    assertEquals(em.getBestResultFromStudent("neri"), Some(30))
-//    assertEquals(em.getBestResultFromStudent("viola"), None)
-//  }
-//
-//
-//  @Test(expected = classOf[IllegalArgumentException])
-//  def optionalTestCantCreateACallTwice() {
-//    this.prepareExams()
-//    em.createNewCall("marzo")
-//  }
-//
-//  @Test(expected = classOf[IllegalArgumentException])
-//  def optionalTestCantRegisterAnEvaluationTwice() {
-//    this.prepareExams()
-//    em.addStudentResult("gennaio", "verdi", erf.failed())
-//  }
+  // metodo di creazione di una situazione di risultati in 3 appelli
+  def prepareExams() {
+    em.createNewCall("gennaio")
+    em.createNewCall("febbraio")
+    em.createNewCall("marzo")
+
+    em.addStudentResult("gennaio", "rossi", erf.failed()) // rossi -> fallito
+    em.addStudentResult("gennaio", "bianchi", erf.retired()) // bianchi -> ritirato
+    em.addStudentResult("gennaio", "verdi", erf.succeeded(28)) // verdi -> 28
+    em.addStudentResult("gennaio", "neri", erf.succeededCumLaude()) // neri -> 30L
+
+    em.addStudentResult("febbraio", "rossi", erf.failed()) // etc..
+    em.addStudentResult("febbraio", "bianchi", erf.succeeded(20))
+    em.addStudentResult("febbraio", "verdi", erf.succeeded(30))
+
+    em.addStudentResult("marzo", "rossi", erf.succeeded(25))
+    em.addStudentResult("marzo", "bianchi", erf.succeeded(25))
+    em.addStudentResult("marzo", "viola", erf.failed())
+  }
+
+  // verifica base della parte obbligatoria di ExamManager
+  @Test
+  def testExamsManagement() {
+    this.prepareExams()
+
+    // partecipanti agli appelli di gennaio e marzo
+    assertEquals(em.getAllStudentsFromCall("gennaio"), HashSet("rossi","bianchi","verdi","neri"))
+    assertEquals(em.getAllStudentsFromCall("marzo"), HashSet("rossi","bianchi","viola"))
+
+    // promossi di gennaio con voto
+    assertEquals(em.getEvaluationsMapFromCall("gennaio").size,2)
+    assertEquals(em.getEvaluationsMapFromCall("gennaio").get("verdi"), Some(28))
+    assertEquals(em.getEvaluationsMapFromCall("gennaio").get("neri"), Some(30))
+    // promossi di febbraio con voto
+    assertEquals(em.getEvaluationsMapFromCall("febbraio").size,2);
+    assertEquals(em.getEvaluationsMapFromCall("febbraio").get("bianchi"), Some(20))
+    assertEquals(em.getEvaluationsMapFromCall("febbraio").get("verdi"), Some(30))
+
+    // tutti i risultati di rossi (attenzione ai toString!!)
+    assertEquals(em.getResultsMapFromStudent("rossi").size,3)
+    assertEquals(em.getResultsMapFromStudent("rossi")("gennaio"),"FAILED")
+    assertEquals(em.getResultsMapFromStudent("rossi")("febbraio"),"FAILED")
+    assertEquals(em.getResultsMapFromStudent("rossi")("marzo"),"SUCCEEDED(25)")
+
+    // tutti i risultati di bianchi
+    assertEquals(em.getResultsMapFromStudent("bianchi").size,3)
+    assertEquals(em.getResultsMapFromStudent("bianchi")("gennaio"),"RETIRED")
+    assertEquals(em.getResultsMapFromStudent("bianchi")("febbraio"),"SUCCEEDED(20)")
+    assertEquals(em.getResultsMapFromStudent("bianchi")("marzo"),"SUCCEEDED(25)")
+
+    // tutti i risultati di neri
+    assertEquals(em.getResultsMapFromStudent("neri").size,1)
+    assertEquals(em.getResultsMapFromStudent("neri")("gennaio"),"SUCCEEDED(30L)")
+  }
+
+  // verifica del metodo ExamManager.getBestResultFromStudent
+  @Test
+  def optionalTestExamsManagement() {
+    this.prepareExams();
+    // miglior voto acquisito da ogni studente, o vuoto..
+    assertEquals(em.getBestResultFromStudent("rossi"), Some(25))
+    assertEquals(em.getBestResultFromStudent("bianchi"), Some(25))
+    assertEquals(em.getBestResultFromStudent("neri"), Some(30))
+    assertEquals(em.getBestResultFromStudent("viola"), None)
+  }
+
+
+  @Test(expected = classOf[IllegalArgumentException])
+  def optionalTestCantCreateACallTwice() {
+    this.prepareExams()
+    em.createNewCall("marzo")
+  }
+
+  @Test(expected = classOf[IllegalArgumentException])
+  def optionalTestCantRegisterAnEvaluationTwice() {
+    this.prepareExams()
+    em.addStudentResult("gennaio", "verdi", erf.failed())
+  }
 }
